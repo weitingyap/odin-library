ce36efd99b703a008ee8f360fc11a074bcf322d1
Wei-Ting Yap <29475856+weitingyap@users.noreply.github.com>
2025-01-26 14:38:18 +0800

    feat: populate library with four books

--- script.js
+++ script.js
@@ -14,3 +14,8 @@ function Book(title, author, nPages, readStatus = 'not read yet'){
     const book = new Book(title, author, nPages, readStatus);
     myLibrary.push(book);
   }
+
+addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295);
+addBookToLibrary('Norwegian Wood', 'Haruki Murakami', 296);
+addBookToLibrary('1Q84', 'Haruki Murakami', 925);
+addBookToLibrary('The City and Its Uncertain Walls', 'Haruki Murakami', 464);

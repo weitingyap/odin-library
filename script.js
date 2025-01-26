4c6b2679bcf14cff9bf0d7a2cb5dfcddc2af6c93
Wei-Ting Yap <29475856+weitingyap@users.noreply.github.com>
2025-01-26 14:32:52 +0800

    feat: add Book constructor to create books

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("hello world!");
+const myLibrary = [];
+
+function Book(title, author, nPages, readStatus = 'not read yet'){
+    this.title = title; 
+    this.author = author;
+    this.nPages = nPages;
+    this.readStatus = readStatus;
+    this.info = function(){
+      return `${this.title} by ${this.author}, ${this.nPages} pages, ${this.readStatus}`;
+    };
+  }

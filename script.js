7fca16c947d66351cdd183a799b8dde45c4d4a2e
Wei-Ting Yap <29475856+weitingyap@users.noreply.github.com>
2025-02-01 09:44:14 +0800

    feat: populate table with books from library array

--- script.js
+++ script.js
@@ -19,3 +19,21 @@ addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295);
 addBookToLibrary('Norwegian Wood', 'Haruki Murakami', 296);
 addBookToLibrary('1Q84', 'Haruki Murakami', 925);
 addBookToLibrary('The City and Its Uncertain Walls', 'Haruki Murakami', 464);
+
+// populate table with existing books in library
+
+table = document.querySelector("#library-books tbody");
+row = document.createElement("tr");
+cell = document.createElement("td");
+
+for (let book of myLibrary){
+  newRow = row.cloneNode();
+  for (let prop in book){
+    if (typeof book[prop] != 'function'){ // skip over object methods
+      newCell = cell.cloneNode();
+      newCell.innerText = book[prop];
+      newRow.appendChild(newCell);
+    }
+  }
+  table.appendChild(newRow);
+}
